9fc9eea0b885ea36a8d4dce67621d02ce923124f
koraykocakaya <mkoraykocakaya@gmail.com>
2021-03-09 14:21:59 +0300

    app, app-test fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there, selam22ss
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          I was changed on the Feature branch
         </p>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
